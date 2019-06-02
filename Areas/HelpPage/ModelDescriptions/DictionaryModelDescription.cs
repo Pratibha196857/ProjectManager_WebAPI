6fe95e0bc6b4134e2ad4667c25d0ae87bef04ec2
Pratibha Unnithan <pratibha.unnithan@cognizant.com>
2019-06-02 17:16:11 +0530

    nitial commit

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProjectManager_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ obj/Release/AspnetCompileMerge/Source/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectManager_WebAPI.WebApiApplication" Language="C#" %>
